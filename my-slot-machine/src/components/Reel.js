921fcea1c72bc26703765cb7ea8fc2a0b1e8cb8a
varuunnn216 <varuunnn216@gmail.com>
2024-11-23 11:57:57 +0530

    changes added to reels.js

--- my-slot-machine/src/components/Reel.js
+++ my-slot-machine/src/components/Reel.js
@@ -4,7 +4,7 @@ import '../styles/SlotMachine.css';
 
 const Reel = ({ item, spinning }) => {
   return (
-    <div className={`reel ${spinning ? 'spinning....' : ''}`}>
+    <div className={`reel ${spinning ? 'spinning.....' : ''}`}>
       <div className="item">
         <FontAwesomeIcon icon={item.icon} size="2x" className="item-icon" />
         <h4>{item.type}</h4>
